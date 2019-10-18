59be0338945c11d976264d8fd71df8584769b26a
John Bush <fbush@pivotal.io>
2019-10-18 13:42:52 -0500

    GetHeadlinesCommand's fallback tries to return cached result

--- BreakingNewsWeb/Program.cs
+++ BreakingNewsWeb/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 
@@ -15,12 +17,26 @@ namespace BreakingNewsWeb
         private static IWebHost BuildWebHost(string[] args)
         {
             var builder = WebHost.CreateDefaultBuilder(args);
-            builder = builder.AddConfigServer();
 
-            return CloudFoundryHostBuilderExtensions.UseCloudFoundryHosting(builder)
-                .AddCloudFoundry()
-                .UseStartup<Startup>()
-                .Build();
+            // retrieve configuration from Config Server
+            builder = builder.AddConfigServer(GetLoggerFactory());
+
+            // use the "PORT" env variable to set the port to listen on
+            builder = CloudFoundryHostBuilderExtensions.UseCloudFoundryHosting(builder);
+
+            // add VCAP_* configuration data
+            builder = CloudFoundryHostBuilderExtensions.AddCloudFoundry(builder);
+
+            return builder.UseStartup<Startup>().Build();
+        }
+
+        private static ILoggerFactory GetLoggerFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Trace));
+            serviceCollection.AddLogging(builder => builder.AddConsole((opts) => { opts.DisableColors = true; }));
+            serviceCollection.AddLogging(builder => builder.AddDebug());
+            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
         }
     }
 }
